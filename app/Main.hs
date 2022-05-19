bf3b1ca4a4f9aef2da5174694f4b5db1135b6da1
Norbert Dzikowski <norbert@perun.network>
2022-05-19 02:50:20 +0200

    Dzang/App: Update usage to new interpreter abstraction

--- app/Main.hs
+++ app/Main.hs
@@ -1,83 +1,35 @@
-{-# LANGUAGE BangPatterns #-}
-
 module Main where
 
-import Control.Exception
-import Dzang.Interpreter
-import Dzang.Language
-import Dzang.Typing.TypeChecker
-import Dzang.Typing.Types
-import Options.Applicative
+import           Control.Monad                  ( void )
+import           Dzang.Interpreter
+import           Options.Applicative
 
 data CmdLineArgs = CLA
-  { debug :: Bool,
-    eval :: Bool,
-    parse :: Bool,
-    typeIt :: Bool
+  { interpret :: Bool
+  , debug      :: Bool
   }
-
-data InterpreterResult = IR Value PolyType
-
-instance Show InterpreterResult where
-  show (IR v pt) = show v <> ": " <> show pt
 
 main :: IO ()
 main = execParser opts >>= main'
-  where
-    opts =
-      info
-        (cmdLineParser <**> helper)
-        (fullDesc <> progDesc "Dzang interpreter environment")
+ where
+  opts = info (cmdLineParser <**> helper)
+              (fullDesc <> progDesc "Dzang interpreter environment")
 
 main' :: CmdLineArgs -> IO ()
-main' (CLA False False False False) = forever [] undefined
-main' (CLA True _ _ _) = forever [] undefined
-main' (CLA _ True _ _) = forever [] undefined
-main' (CLA _ _ True _) = forever [] undefined
-main' (CLA _ _ _ True) =
-  forever
-    []
-    ( \s env ->
-        let expr = parseDzang s
-            !pt = runTypeChecker [] expr
-            res = evalInterpreter expr env
-         in do
-              pretty $ IR res pt
-              return env
-    )
-
-pretty :: Show a => a -> IO ()
-pretty a = putStrLn $ "▶ " <> show a
-
-forever :: Environment -> (String -> Environment -> IO Environment) -> IO ()
-forever env f = do
-  l <- getLine
-  res <- try (f l env) :: IO (Either SomeException Environment)
-  case res of
-    Left err -> print err >> forever env f
-    Right env' -> forever env' f
+main' (CLA False False) = void $ runInterpreter forever emptyInterpreterState
+main' (CLA True  _    ) = undefined
+main' (CLA _     True ) = undefined
 
 cmdLineParser :: Parser CmdLineArgs
 cmdLineParser =
   CLA
     <$> switch
-      ( long "debug"
+          (long "interpret" <> short 'i' <> help
+            "Dzang interpreter running in default mode, parsing input and returning their result and type."
+          )
+    <*> switch
+          (  long "debug"
           <> short 'd'
           <> help
-            "Debugmode for the Dzang parser printing the resulting parser state"
-      )
-    <*> switch
-      ( long "eval" <> short 'e'
-          <> help
-            "Dzang evaluator interpreting user input"
-      )
-    <*> switch
-      ( long "parse" <> short 'p'
-          <> help
-            "Dzang parser returning the result of a parse operation"
-      )
-    <*> switch
-      ( long "type" <> short 't'
-          <> help
-            "Dzang parser returning the type of the given expression"
-      )
+               "Debugmode for the Dzang parser printing the resulting parser state."
+          )
